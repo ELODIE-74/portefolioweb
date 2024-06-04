4373a8289614621d760ad44ebd1f6df1ebc16e8e
ELODIE-74 <142718693+ELODIE-74@users.noreply.github.com>
2024-06-04 10:01:47 +0000

    configuration color web site

--- my-portfolio-app/src/components/footer/footer.jsx
+++ my-portfolio-app/src/components/footer/footer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import "./footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p>&#x24B8; 2020 kasa. All rights reserved</p>
+      <div className="footer-content">
+        <p>&#x24B8; 2020 kasa. All rights reserved</p>
+      </div>
     </footer>
   );
 }
