ea1e0150212a0df021b90c6407b678b8471d9e49
ELODIE-74 <142718693+ELODIE-74@users.noreply.github.com>
2024-06-15 15:31:15 +0000

    essai loader et modif design

--- my-portfolio-app/src/components/loader/loader.jsx
+++ my-portfolio-app/src/components/loader/loader.jsx
@@ -5,7 +5,7 @@ const Loader = () => {
   return (
     <div className="loader">
       <div className="loaderConteneur">
-        <h1>Portfolio d'Elodie</h1>
+        <h1>PORTFOLIO</h1>
         <div className="loaderConteneurChargement"></div>
       </div>
     </div>
