29baeb6cde96fcbd1426be0d6a91655cef394987
ELODIE-74 <142718693+ELODIE-74@users.noreply.github.com>
2024-06-15 16:06:42 +0000

    header design valider

--- my-portfolio-app/src/App.js
+++ my-portfolio-app/src/App.js
@@ -4,11 +4,6 @@ import Header from './components/header/header.jsx';
 import Footer from './components/footer/footer.jsx';
 import Home from './pages/home/home.jsx';
 import Error404 from './pages/error404/error404.jsx';
-import Contact from './pages/contact/contact.jsx';
-import Projets from './pages/projets/projets.jsx';
-import Presentation from './pages/presentation/presentation.jsx';
-import Competences from './pages/competences/competences.jsx';
-import CVViewer from './pages/cv/cv.jsx';
 import Loader from './components/loader/loader.jsx'; // Importez le composant Loader
 
 function App() {
@@ -28,11 +23,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Error404 />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="projets" element={<Projets />} />
-        <Route path="presentation" element={<Presentation />} />
-        <Route path="competences" element={<Competences />} />
-        <Route path="cv" element={<CVViewer />} />
       </Routes>
       <Footer />
     </Router>
--- my-portfolio-app/src/components/header/header.jsx
+++ my-portfolio-app/src/components/header/header.jsx
@@ -1,5 +1,61 @@
+import React, { useState, useRef } from 'react';
+import { Link } from "react-router-dom";
+import "./header.css";
 
-import React, { useRef } from 'react'; // Importez useRef ici
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const presentationSectionRef = useRef(null);
+  const projetsSectionRef = useRef(null);
+  const competencesSectionRef = useRef(null);
+  const cvSectionRef = useRef(null);
+  const contactSectionRef = useRef(null);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const scrollToSection = (sectionRef) => {
+    sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    toggleMenu();
+  };
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <i className="fa-solid fa-laptop"></i>
+        <span className='textelogo'>Portfolio d'Elodie</span>
+      </div>
+      <nav className={`menu ${showMenu ? 'show' : ''}`}>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <i className="fas fa-bars"></i>
+        </div>
+        <ul className="menu-list">
+          <li>
+            <Link to="/" onClick={toggleMenu}>Accueil</Link>
+          </li>
+          <li>
+            <Link to="/presentation" onClick={() => scrollToSection(presentationSectionRef)}>Présentation</Link>
+          </li>
+          <li>
+            <Link to="/projets" onClick={() => scrollToSection(projetsSectionRef)}>Projets</Link>
+          </li>
+          <li>
+            <Link to="/competences" onClick={() => scrollToSection(competencesSectionRef)}>Compétences</Link>
+          </li>
+          <li>
+            <Link to="/cv" onClick={() => scrollToSection(cvSectionRef)}>CV</Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={() => scrollToSection(contactSectionRef)}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
+/*import React, { useRef } from 'react'; // Importez useRef ici
 import { Link } from "react-router-dom";
 import "./header.css";
 
@@ -41,4 +97,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default Header;*/
